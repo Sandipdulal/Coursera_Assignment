d7fc39df756dada0250e523c55fdeaa36cff8858
Sandip Dulal <dulalsandip@gmail.com>
2016-10-22 00:48:24 -0500

    'mod4_solution'

--- mod3_solution/app.js
+++ mod3_solution/app.js
@@ -1,87 +1,89 @@
-    (function () {
-    'use strict';
+    (function() {
+        'use strict';
 
-    angular.module('NarrowItDownApp',[])
-    .controller('NarrowItDownController',NarrowItDownController)
-    .service('MenuSearchService',MenuSearchService)
-    .constant('ApiBasePath'," https://davids-restaurant.herokuapp.com/menu_items.json")
-    .directive('foundItems',FoundItems);
-
-
-    function FoundItems(){
-
-      var ddo={
-        templateUrl:'foundItems.html',
-        scope:{
-          foundItemsList:'<',
-          onRemove:'&'
-        },
-        controller:NarrowItDownController,
-        controllerAs:'searchCtrl',
-        bindToController:true
-      };
-      return ddo;
-    }
+        angular.module('NarrowItDownApp', [])
+            .controller('NarrowItDownController', NarrowItDownController)
+            .service('MenuSearchService', MenuSearchService)
+            .constant('ApiBasePath', " https://davids-restaurant.herokuapp.com/menu_items.json")
+            .directive('foundItems', FoundItems);
 
 
-    NarrowItDownController.$inject=['MenuSearchService','$scope'];
-    function NarrowItDownController(MenuSearchService,$scope) {
-      var menu_items=this;
-      menu_items.foundArray=[];
-      menu_items.searchTermItems=function(searchItems){
+        function FoundItems() {
 
-        menu_items.foundArray = MenuSearchService.getMatchedMenuItems(searchItems);
+            var ddo = {
+                templateUrl: 'foundItems.html',
+                scope: {
+                    foundItemsList: '<',
+                    onRemove: '&'
+                },
+                controller: NarrowItDownController,
+                controllerAs: 'searchCtrl',
+                bindToController: true
+            };
+            return ddo;
+        }
 
-              menu_items.foundArray.then(function(response){
 
-                  menu_items.found=response;
+        NarrowItDownController.$inject = ['MenuSearchService'];
 
-                      }).catch(function(error){
-                              console.log("Something went terribly wrong");
+        function NarrowItDownController(MenuSearchService) {
+            var menu_items = this;
+            menu_items.foundArray = [];
+            menu_items.searchTermItems = function(searchItems) {
+
+                menu_items.foundArray = MenuSearchService.getMatchedMenuItems(searchItems);
+
+                menu_items.foundArray.then(function(response) {
+
+                    menu_items.found = response;
+
+                }).catch(function(error) {
+                    console.log("Something went terribly wrong");
                 });
 
 
 
-              };
-
-               menu_items.removeItem=function(itemIndex){
-                menu_items.found.splice(itemIndex,1);
-               };
             };
 
-    MenuSearchService.$inject=['$http','ApiBasePath'];
-    function MenuSearchService($http,ApiBasePath){
-      var service=this;
-      service.getMatchedMenuItems=function(searchTerm){
+            menu_items.removeItem = function(itemIndex) {
+                menu_items.found.splice(itemIndex, 1);
+            };
+        };
 
-        return $http({
-    method:"GET",
-    url:ApiBasePath,
+        MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 
-            }).then(function (result) {
-          // process result and only keep items that match
+        function MenuSearchService($http, ApiBasePath) {
+            var service = this;
+            service.getMatchedMenuItems = function(searchTerm) {
 
-                service.foundItems=[];
+                return $http({
+                    method: "GET",
+                    url: ApiBasePath,
 
-                      angular.forEach(result.data,function(values,key){
-                          angular.forEach(values,function(value,key){
-                              if((value.description.indexOf(searchTerm)!==-1)&& searchTerm!=""){
-                                  service.foundItems.push(value);
+                }).then(function(result) {
+                    // process result and only keep items that match
 
-                                };
+                    service.foundItems = [];
 
-                              });
+                    angular.forEach(result.data, function(values, key) {
+                        angular.forEach(values, function(value, key) {
+                            if ((value.description.indexOf(searchTerm) !== -1) && searchTerm != "") {
+                                service.foundItems.push(value);
 
-                            });
+                            };
 
-          // return processed items
+                        });
 
-          return service.foundItems;
-      })
+                    });
 
-      }
+                    // return processed items
+
+                    return service.foundItems;
+                })
+
+            }
 
 
-    };
+        };
 
     })();
---
+++ mod4_solution/src/shoppinglist/main-shoppinglist.controller.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular.module('Data')
+        .controller('MainShoppingListController', MainShoppingListController);
+
+
+    MainShoppingListController.$inject = ['items'];
+
+    function MainShoppingListController(items) {
+        var mainList = this;
+        mainList.items = items.data;
+    }
+
+})();
